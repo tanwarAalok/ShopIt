5b22b92bc21cd73d69c18736075573063bb99370
Aalok Tanwar <aaloktanwar88@gmail.com>
2023-01-05 14:20:39 +0530

    cart toggle updated

--- src/components/cart-dropdown/cart-dropdown.component.jsx
+++ src/components/cart-dropdown/cart-dropdown.component.jsx
@@ -2,15 +2,18 @@ import { CartDropdownContainer, EmptyMessage, CartItems } from "./cart-dropdown.
 import Button from '../button/button.component';
 import CartItem from '../cart-item/cart-item.component';
 import { useNavigate } from 'react-router-dom';
-import { useSelector } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import { selectCartItems } from "../../store/cart/cart.select";
+import { setIsCartOpen } from "../../store/cart/cart.action";
 
 const CartDropDown = () => {
   const navigate = useNavigate();
   const cartItems = useSelector(selectCartItems);
+  const dispatch = useDispatch();
 
   const goToCheckoutHandler = () => {
     navigate('/checkout');
+    dispatch(setIsCartOpen(false));
   }
 
     return (
@@ -25,7 +28,7 @@ const CartDropDown = () => {
           }
   
         </CartItems>
-        <Button onClick={goToCheckoutHandler} >GO TO CHECKOUT</Button>
+        <Button onClick={goToCheckoutHandler}>GO TO CHECKOUT</Button>
       </CartDropdownContainer>
     );
 };
